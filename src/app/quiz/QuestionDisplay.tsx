2eb10e75b2fd00176fc954569d37e3bcdfe730a5
hyudai28 <hayasakayudai@gmail.com>
2024-08-18 17:13:38 +0900

    placeholderを導入

--- src/app/quiz/QuestionDisplay.tsx
+++ src/app/quiz/QuestionDisplay.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import { Message } from "@freee_jp/vibes";
+import { placeholderQuestions } from "@/app/quiz/data/placeholders/questions";
 
 export default function QuestionDisplay() {
+  const [currentQuestion, setCurrentQuestion] = useState(
+    placeholderQuestions[1]
+  );
+
   return (
     <div>
-      <Message>問題、カラスの色は白いですか？</Message>
+      <Message>{currentQuestion.question}</Message>
     </div>
   );
 }
---
+++ src/app/quiz/data/placeholders/questions.tsx
@@ -0,0 +1,4 @@
+export const placeholderQuestions = [
+  { id: 1, question: "カラスの色は白いですか？" },
+  { id: 2, question: "海の色は赤いですか？" },
+];
